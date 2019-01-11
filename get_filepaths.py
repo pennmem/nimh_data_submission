2596fb3a3364bbafb2a1be0689c396aabe01b9a7
jpazdera <jpazdera@haverford.edu>
2019-01-11 17:05:40 -0500

    Completed implementation of file path identification

--- get_filepaths.py
+++ get_filepaths.py
@@ -1,8 +1,18 @@
 import os
 import glob
+import pandas as pd
 
 
 def get_filepaths(info, ltp_path='/data/eeg/scalp/ltp/', protocols_path='/protocols/ltp/'):
+
+    info['data_file1'] = pd.Series(None, index=info.index)
+    info['data_file1_type'] = pd.Series(None, index=info.index)
+    info['data_file2'] = pd.Series(None, index=info.index)
+    info['data_file2_type'] = pd.Series(None, index=info.index)
+    info['data_file3'] = pd.Series(None, index=info.index)
+    info['data_file3_type'] = pd.Series(None, index=info.index)
+    info['data_file4'] = pd.Series(None, index=info.index)
+    info['data_file4_type'] = pd.Series(None, index=info.index)
 
     for i, sess_data in info.iterrows():
 
@@ -10,8 +20,8 @@ def get_filepaths(info, ltp_path='/data/eeg/scalp/ltp/', protocols_path='/protoc
         exp = sess_data.experiment
         subj = sess_data.subject
         sess = sess_data.session
-        sess_path = os.path.join(ltp_path, exp, subj, '/session_' + sess)
-        db_path = os.path.join(protocols_path, 'subjects', subj, 'experiments', exp, 'sessions', sess)
+        sess_path = os.path.join(ltp_path, '%s/%s/session_%s' % (exp, subj, sess))
+        db_path = os.path.join(protocols_path, 'subjects/%s/experiments/%s/sessions/%s' % (subj, exp, sess))
 
         # Determine the filepaths and file types for the current session
         if exp == 'pyFR':
@@ -26,7 +36,8 @@ def get_filepaths(info, ltp_path='/data/eeg/scalp/ltp/', protocols_path='/protoc
             filepaths = ['' for _ in range(8)]
 
         # Add file information to the data frame
-        pass
+        info.loc[i, ('data_file1', 'data_file1_type', 'data_file2', 'data_file2_type',
+                     'data_file3', 'data_file3_type', 'data_file4', 'data_file4_type')] = filepaths
 
     return info
 
@@ -91,6 +102,19 @@ def get_filepaths_SFR(sess_path, ltp_path, exp, subj):
 
 def get_filepaths_VFFR(sess_path, db_path):
 
+    eeg_files = glob.glob(os.path.join(db_path, 'ephys', 'current_processed', '*.bdf'))
+    eeg_files = [''] if len(eeg_files) == 0 else eeg_files
+    events_file = os.path.join(db_path, 'behavioral', 'current_processed', 'task_events.json')
+    session_log = os.path.join(sess_path, 'session.jsonl')
+
     filepaths = ['' for _ in range(8)]
+    filepaths[0] = eeg_files[0]
+    filepaths[1] = 'EEG' if filepaths[0] != '' else ''
+    filepaths[2] = events_file if os.path.exists(events_file) else session_log
+    filepaths[3] = 'Behavioral' if os.path.exists(events_file) else 'Session Log'
+    filepaths[4] = eeg_files[1] if len(eeg_files) > 1 else ''
+    filepaths[5] = 'EEG' if filepaths[4] != '' else ''
+    filepaths[6] = eeg_files[2] if len(eeg_files) > 2 else ''
+    filepaths[7] = 'EEG' if filepaths[6] != '' else ''
 
     return filepaths
