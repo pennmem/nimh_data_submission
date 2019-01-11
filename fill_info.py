2e460fdb505f5cdbd1deab6cc8815d2db45eb7f7
jpazdera <jpazdera@haverford.edu>
2019-01-11 13:00:06 -0500

    Added placeholders for all fields that should be filled

--- fill_info.py
+++ fill_info.py
@@ -1,11 +1,63 @@
+import numpy as np
 import pandas as pd
+
 
 def fill_info(ed, esf, info):
 
-    ed = ed.append(pd.DataFrame(None, index=info.index))
-    esf = esf.append(pd.DataFrame(None, index=info.index))
-
-    ed.loc[:, 'src_subject_id'] = info.subject
-    esf.loc[:, 'src_subject_id'] = info.subject
+    ed = fill_eeg_details(ed, info)
+    esf = fill_eeg_sub_files(esf, info)
 
     return ed, esf
+
+
+def fill_eeg_details(ed, info):
+
+    ed = ed.append(pd.DataFrame(None, index=info.index))
+    ed.loc[:, 'subjectkey'] = np.nan  # Subject GUID
+    ed.loc[:, 'src_subject_id'] = info.subject  # Subject ID
+    ed.loc[:, 'interview_date'] = np.nan  # Session date (MM/DD/YYYY)
+    ed.loc[:, 'interview_age'] = info.age_in_months  # Participant's age in months
+    ed.loc[:, 'gender'] = info.gender  # Participant's gender
+    ed.loc[:, 'visit'] = info.session  # Session number
+    ed.loc[:, 'eeg001'] = 1  # 1 if EEG was used, 0 if no EEG
+    ed.loc[:, 'eeg003b'] = np.nan  # Handedness for throwing a ball (1-5)
+    ed.loc[:, 'eeg003d'] = np.nan  # Handedness for brushing teeth (1-5)
+    ed.loc[:, 'eeg003e'] = np.nan  # Handedness for using scissors (1-5)
+    ed.loc[:, 'eeg003g'] = np.nan  # Handedness for writing (1-5)
+    ed.loc[:, 'eeg008c'] = np.nan  # Hours of sleep
+    ed.loc[:, 'eeg008d'] = np.nan  # Alertness (1-5)
+    ed.loc[:, 'eeg013'] = np.nan  # Start time of EEG recording
+    ed.loc[:, 'head_circum'] = np.nan  # Participant's head circumference (in cm)
+    ed.loc[:, 'eeg015'] = np.nan  # Size of cap used (PM, PL, AS, AM, AML, AL, AXL)
+    ed.loc[:, 'eeg022'] = np.nan  # End time of EEG recording
+    ed.loc[:, 'eeg026'] = 0  # 1 if task included faces, 0 if not
+    ed.loc[:, 'eeg027'] = 0  # 1 if task involved resting with eyes closed, 0 if not
+    ed.loc[:, 'eeg028'] = 0  # 1 if a cognitive flanker task, 0 if not
+    ed.loc[:, 'eeg029'] = 0  # 1 if task involved resting with eyes open, 0 if not
+    ed.loc[:, 'eeg_4'] = 1  # 1 if participant had normal or correct-to-normal vision
+
+    return ed
+
+
+def fill_eeg_sub_files(esf, info):
+
+    esf = esf.append(pd.DataFrame(None, index=info.index))
+    esf.loc[:, 'subjectkey'] = np.nan  # Subject GUID
+    esf.loc[:, 'src_subject_id'] = info.subject  # Subject ID
+    esf.loc[:, 'interview_date'] = np.nan  # Session date (MM/DD/YYYY)
+    esf.loc[:, 'interview_age'] = info.age_in_months  # Participant's age in months
+    esf.loc[:, 'gender'] = info.gender  # Participant's gender
+    esf.loc[:, 'ofc'] = np.nan  # Participant's head circumference (in cm)
+    esf.loc[:, 'experiment_id'] = np.nan  # Experiment's ID number
+    esf.loc[:, 'data_file1'] = np.nan  # Data file 1 (typically EEG recording)
+    esf.loc[:, 'data_file1_type'] = np.nan  # File type of file 1 (typically "EEG")
+    esf.loc[:, 'data_file2'] = np.nan  # Data file 2 (typically events file)
+    esf.loc[:, 'data_file2_type'] = np.nan  # File type of file 2 (typically "Behavioral")
+    esf.loc[:, 'data_file3'] = np.nan  # Data file 3 (extra file, e.g. a sync pulse log or second EEG recording)
+    esf.loc[:, 'data_file3_type'] = np.nan  # File type of file 3
+    esf.loc[:, 'data_file4'] = np.nan  # Data file 4 (extra file, e.g. a sync pulse log or second EEG recording)
+    esf.loc[:, 'data_file4_type'] = np.nan  # File type of file 4
+    esf.loc[:, 'head_circum'] = np.nan  # Participant's head circumference (in cm)
+    esf.loc[:, 'visit'] = info.session  # Session number
+
+    return esf
